184da16baa77838b2a6d3360f7c6049a39e34c5d
Vinod Siraparapu <vinodsiraparapu@Vinods-MacBook-Pro.local>
2023-10-22 16:38:57 +0530

    updated conversions

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const ENV = 'DEV';
 const app = express();
 app.get('/', (req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello from from Primus Learning, this was awesome';
+  const msg = 'Hello from from IDP team, this was awesome';
   res.send(getPage(msg));
 });
 
